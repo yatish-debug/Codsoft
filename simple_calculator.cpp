a127d16b9a76f21fcad85797069fa4c13be0a1bb
Yatish Hemant Bharambe <yhbharmbe26@gmail.com>
2025-01-14 10:13:43 +0530

    Add files via upload

---
+++ simple_calculator.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    long n1,n2;
+    char oprator;
+    cout<<"Enter The Number 1=";
+    cin>>n1;
+    cout<<"Enter The Number 2=";
+    cin>>n2;
+    cout<<"Enter The Operator=";
+    cin>>oprator;
+    switch(oprator)
+    {
+        case '+':
+        cout<<"Sum="<<n1+n2<<endl;
+        break;
+        case '-':
+        cout<<"Subtract="<<n1-n2<<endl;
+        break;
+        case '*':
+        cout<<"Multiplication="<<n1*n2<<endl;
+        break;
+        case '/':
+        if(n2==0)
+        {
+            cout<<"This Number Is Not A Valid Input Because The Operation Is Not Valid";
+        }
+        else 
+        {
+            cout<<"Result ="<<n1/n2<<endl;
+        }
+        break;
+        default:
+        cout<<"Operation Is Not Valid:Pls Try New Operation";
+        getch();
+    }
+}
